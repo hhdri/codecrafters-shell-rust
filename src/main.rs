77217b4fd75fa835723746ce994f7edc9beef2d1
Majid Hajiheidari <majid.hajiheidari@teampicnic.com>
2025-12-02 21:23:27 +0100

    Handle invalid commands

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,15 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
 
-fn main() {
-    // TODO: Uncomment the code below to pass the first stage
+fn main() -> io::Result<()> {
     print!("$ ");
-    io::stdout().flush().unwrap();
+    io::stdout().flush()?;
+
+    let mut buffer = String::new();
+    io::stdin().read_line(&mut buffer)?;
+    buffer = buffer[..(buffer.len() - 1)].parse().unwrap();
+
+    println!("{}: command not found", buffer);
+
+    Ok(())
 }
