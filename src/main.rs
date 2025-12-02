5238d1b523ddc5dd7ff0f3cdd799f53be0eb6273
Majid Hajiheidari <majid.hajiheidari@teampicnic.com>
2025-12-02 21:27:53 +0100

    Implement a REPL

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,16 @@
 use std::io::{self, Write};
 
 fn main() -> io::Result<()> {
-    print!("$ ");
-    io::stdout().flush()?;
+    loop {
+        print!("$ ");
+        io::stdout().flush()?;
 
-    let mut buffer = String::new();
-    io::stdin().read_line(&mut buffer)?;
-    buffer = buffer[..(buffer.len() - 1)].parse().unwrap();
+        let mut buffer = String::new();
+        io::stdin().read_line(&mut buffer)?;
+        buffer = buffer[..(buffer.len() - 1)].parse().unwrap();
 
-    println!("{}: command not found", buffer);
+        println!("{}: command not found", buffer);
+    }
 
-    Ok(())
+    // Ok(())
 }
